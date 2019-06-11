f78da013db595f553b1d711153ba8c1a4a44a35e
italoboss <italoboss@gmail.com>
2019-06-11 10:51:11 -0300

    Initial Commit

---
+++ EmotionalDiary/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  EmotionalDiary
+//
+//  Created by Italo Boss on 11/06/19.
+//  Copyright © 2019 Italo Boss. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
